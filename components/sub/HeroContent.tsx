b2fffb29d6742d370db6ac613fecdeba9b604cab
Mohammed Ajmal Shan <69510133+mohammedajmalshan@users.noreply.github.com>
2024-09-15 13:50:47 +0530

    Update HeroContent.tsx

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -31,7 +31,7 @@ const HeroContent = () => {
           className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
         >
           <span>
-            Hello, I&apos;M Mohammed
+            Hello, I&apos;M
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
               {" "}
               Ajmal{" "}
