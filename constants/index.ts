c6c48e33b540fb789c473d1dd4104adb0a652d59
Mohammed Ajmal Shan <69510133+mohammedajmalshan@users.noreply.github.com>
2024-09-16 12:20:56 +0530

    Update index.ts

--- constants/index.ts
+++ constants/index.ts
@@ -45,16 +45,16 @@ export const Skill_data = [
    
     
     {
-      skill_name: "Node js",
-      Image: "/node-js.png",
-      width: 80,
-      height: 80,
+      skill_name: "",
+      Image: "",
+      width: 0,
+      height: 0,
     },
     {
-      skill_name: "Mongo db",
-      Image: "/mongodb.png",
-      width: 40,
-      height: 40,
+      skill_name: "",
+      Image: "",
+      width: 0,
+      height: 0,
     },
   
   ];
